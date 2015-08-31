2a95787e6a76ce2c2024a28989257757d09e0d60
Ionel Miu <ionel.miu@gmail.com>
2015-08-31 13:56:53 -0700

    inital commit

--- suorganizer/blog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    title = models.CharField(max_length=63)
-    slug = models.SlugField(max_length=63)
-    text = models.TextField()
-    pub_date = models.DateField()
